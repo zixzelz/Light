11ed44b2664af65961860e079e685246d697f14a
Ruslan Maslouski <zixzelz@gmail.com>
2017-08-10 22:06:35 +0300

    Initial Commit

---
+++ Light/Light.ino
@@ -0,0 +1,64 @@
+//
+// Light
+//
+// Light
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		Ruslan Maslouski
+// 				Ruslan Maslouski
+//
+// Date			8/10/17 22:06
+// Version		<#version#>
+//
+// Copyright	© Ruslan Maslouski, 2017
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+#if defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.8 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
---
+++ Light/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  Light
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project Light
+  Created by Ruslan Maslouski on 8/10/17
+  Copyright © 2017 Ruslan Maslouski
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2017
+  All rights reserved
+  http://embedXcode.weebly.com
+
